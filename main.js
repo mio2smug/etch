d580a7df27c588f26c439b10a2c86cf76720d19c
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-26 16:10:41 -0500

    Grid displays the proper number of squares

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let grid = document.querySelector(".grid");
-
-    grid.style.gridTemplateColumns = "repeat (16, 1fr)";
-    grid.style.gridTemplateRows = "repeat (16, 1fr)";
+    createSketchpad(16);
     console.log("Hi");
 });
+
+
+function createSketchpad(int){
+
+    let sketchpad = document.querySelector(".sketchpad");
+    let areaOfSketchpad = (int ** 2);
+
+    sketchpad.style.gridTemplateColumns = `repeat(${int},1fr)`;
+    sketchpad.style.gridTemplateRows = `repeat(${int},1fr)`;
+    
+    for (let i=0; i<areaOfSketchpad; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "white";
+        sketchpad.insertAdjacentElement("beforeend", div );
+    }
+    console.log(areaOfSketchpad);    
+}   
